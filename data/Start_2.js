69b269586bc2f983b40fe2f06e84a5d6f52596fb
Joshua Redman <jpredman@users.noreply.github.com>
2019-04-22 12:21:47 -0500

    updates
    changes from Don's comments

---
+++ data/Start_2.js
@@ -0,0 +1,8 @@
+var json_Start_2 = {
+"type": "FeatureCollection",
+"name": "Start_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Layer": "Race Start" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -87.635140251514841, 41.858525613419438 ], [ -87.635137248653763, 41.858525613419438 ], [ -87.634809936794724, 41.858330427448458 ] ] ] } }
+]
+}
